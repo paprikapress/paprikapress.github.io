78c85147d9eae4a5b282cdf4107d700522c828cb
paprikapress <79066056+paprikapress@users.noreply.github.com>
2022-07-14 20:13:02 +1000

    materials for thermometer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules,RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {ChildComponent} from "./child/child.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {FavoritecolorComponent} from "./favouritecolor/favouritecolor.component";
+import {ThermometerComponent} from "./thermometer/thermometer.component";
+
 
 const routes: Routes = [
   {
@@ -10,21 +12,29 @@ const routes: Routes = [
     component: ChildComponent
   },
 
-  { path: 'favouritecolor',
-    component: FavoritecolorComponent },  // Wildcard route for a 404 page
+  {
+    path: 'favouritecolor',
+    component: FavoritecolorComponent
+  },  // Wildcard route for a 404 page
 
-  { path: 'page',
-    component: PageNotFoundComponent },  // Wildcard route for a 404 page
+
+  {
+    path: 'thermometer34',
+    component: ThermometerComponent
+  },  // Wildcard route for a 404 page
+  {
+    path: 'page',
+    component: PageNotFoundComponent
+  },  // Wildcard route for a 404 page
 
 ];
 
 
-
-
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ChildComponent} from './child/child.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {RouterModule} from "@angular/router";
-import { FavoritecolorComponent } from './favouritecolor/favouritecolor.component';
+import {FavoritecolorComponent} from './favouritecolor/favouritecolor.component';
+import {HelloComponent} from './hello/hello.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from "@angular/material/slider";
+import {MatRadioModule} from "@angular/material/radio";
+import {ThermometerComponent} from './thermometer/thermometer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+
     ChildComponent,
     PageNotFoundComponent,
-    FavoritecolorComponent
+    FavoritecolorComponent,
+    HelloComponent,
+    ThermometerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-
-    RouterModule.forRoot([
-      {path: 'child', component: ChildComponent},
-      {path: 'pageNotFound', component: PageNotFoundComponent},
-      {path: 'favouritecolor', component: FavoritecolorComponent}
-
-    ]),
+    MatRadioModule,
+    MatSliderModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
